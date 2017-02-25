ddf7221be15f8c5b3190d75afd58df23dc734dda
Tien Do <TienDo1011@gmail.com>
2017-02-25 13:11:31 +0700

    Optimize for production use

--- dic_builder.js
+++ dic_builder.js
@@ -9,7 +9,7 @@ require('./models/db');
 var no = 0;
 var numberOfWordAdded = 0;
 
-fs.readFile('./american-english-test', 'utf8', (err, data) => {
+fs.readFile('./american-english', 'utf8', (err, data) => {
   if (err) throw err;
   var arr = data.split('\n');
   arr.forEach(function(item) {
@@ -18,7 +18,6 @@ fs.readFile('./american-english-test', 'utf8', (err, data) => {
       .findOne({word: item})
       .exec(function(err, word) {
         if(!word) {
-          console.log('make request call to oald & 1tudien');
           var oaldWordRequest = item.replace(/'/g, '-');
           var url1 = 'http://www.oxfordlearnersdictionaries.com/definition/english/' + oaldWordRequest;
           var url2 = 'http://dict.laban.vn/find?type=1&query=' + item;
@@ -45,16 +44,12 @@ fs.readFile('./american-english-test', 'utf8', (err, data) => {
               });
             }
           ], function(err, results) {
-            console.log('write Data to dict, item number: ' + [no - 1]);
-            if (no === 101) {
+            if (no === 99171) {
               fs.writeFile('done.txt', `It's done, sir, number of item read ${no}`, (err) => {
                 if (err) throw err;
-                console.log('It\'s saved!');
               });
             }
             if(results[0] !== "" || results[1] !== null) {
-              numberOfWordAdded += 1;
-              console.log('numberOfWordAdded:' + [numberOfWordAdded - 1]);
               DictData.create({
                 word: item,
                 oaldData: results[0],
