81bb753b747a70f2212aa14fb282a0885d788238
Tien Do <TienDo1011@gmail.com>
2017-03-04 14:02:39 +0700

    Add bson file

--- dic_builder.js
+++ dic_builder.js
@@ -1,17 +1,20 @@
 var fs = require('fs');
+var http = require('http');
 var request = require('request');
 var cheerio = require('cheerio');
 var mongoose = require('mongoose');
 var async = require('async');
 var DictData = require('./models/dict-data');
 require('./models/db');
+var pool = new http.Agent;
+pool.maxSockets = 100;
 
 var no = 0;
 var numberOfWordAdded = 0;
 
-fs.readFile('./american-english', 'utf8', (err, data) => {
+fs.readFile('./dicList/5.txt', 'utf8', (err, data) => {
   if (err) throw err;
-  var arr = data.split('\n');
+  var arr = data.split(',');
   arr.forEach(function(item) {
     no += 1;
     DictData
@@ -24,8 +27,11 @@ fs.readFile('./american-english', 'utf8', (err, data) => {
 
           async.parallel([
             function(callback) {
-              request(url1, function(error, response, html) {
+              request({url: url1,
+                pool: pool
+              }, function(error, response, html) {
                 if(!error) {
+                  console.log('Response 1');
                   var $ = cheerio.load(html);
                   callback(null, $.html('.h-g'));
                 }
@@ -35,19 +41,22 @@ fs.readFile('./american-english', 'utf8', (err, data) => {
               request({
                 method: 'GET',
                 url: url2,
+                pool: pool,
                 gzip: true
               }, function(error, response, html) {
                 if(!error) {
+                  console.log('Response 2');
                   var $ = cheerio.load(html);
                   callback(null, $('#content_selectable .content').first().html());
                 }
               });
             }
           ], function(err, results) {
-            if (no === 99171) {
-              fs.writeFile('done.txt', `It's done, sir, number of item read ${no}`, (err) => {
-                if (err) throw err;
-              });
+            if (no === 20000) {
+              // fs.writeFile('done.txt', `It's done, sir, number of item read ${no}`, (err) => {
+              //   if (err) throw err;
+              // });
+              console.log('done');
             }
             if(results[0] !== "" || results[1] !== null) {
               DictData.create({
---
+++ splitWordList.js
@@ -0,0 +1,18 @@
+var fs = require('fs');
+
+fs.readFile('./american-english', 'utf8', (err, data) => {
+  if (err) throw err;
+  const arr = data.split('\n');
+  console.log(arr.length);
+  let i, temparray;
+  const chunk = 20000;
+  const j = arr.length;
+  let no = 0;
+  for(i = 0; i < j; i += chunk) {
+    no += 1;
+    temparray = arr.slice(i, i + chunk);
+    fs.writeFile(`./dicList/${no}.txt`, temparray, (err) => {
+      if (err) throw err;
+    });
+  }
+})
