43c94d180e48c0f459502949604276ba0239938b
tiendo <tiendo1011@gmail.com>
2017-04-26 09:01:34 +0700

    Add wordlist that doesnt include empty field

--- public/javascripts/script.js
+++ public/javascripts/script.js
@@ -3,7 +3,7 @@ let wordlist = new Bloodhound({
   datumTokenizer: Bloodhound.tokenizers.whitespace,
   queryTokenizer: Bloodhound.tokenizers.whitespace,
   // `states` is an array of state names defined in "The Basics"
-  prefetch: '/american-english.json'
+  prefetch: "/no-empty-data.json"
 });
 
 // let substringMatcher = function(strs) {
@@ -28,42 +28,58 @@ let wordlist = new Bloodhound({
 //   };
 // };
 
-$('#s').typeahead(null, {
-  name: 'wordlist',
+$("#s").typeahead(null, {
+  name: "wordlist",
   source: wordlist
 });
 
 $(function() {
   $(document).keydown(function(e) {
-    if(e.which == 13) {
-      var value = $('#s').val().toLowerCase();
-      $('#oald').empty()
-      $('#vndic').empty();
-      $('#spinning').append('<i class="fa fa-spinner fa-pulse fa-5x fa-fw"></i><span class="sr-only">Loading...</span>');
+    if (e.which == 13) {
+      var value = $("#s").val().toLowerCase();
+      $("#oald").empty();
+      $("#vndic").empty();
+      $("#spinning").append(
+        '<i class="fa fa-spinner fa-pulse fa-5x fa-fw"></i><span class="sr-only">Loading...</span>'
+      );
       $.ajax({
         url: "http://dict.tienganhthaytien.com/dict",
         data: { search: value }
-      }).done(function( data ) {
-         $('#spinning').empty();
-	       if(!data.oaldData) {
-      	   $('#oald').append('CANT FIND ANY WORD!');
-         }
-          // Get OALD data
-          var oaldResult = $(data.oaldData);
-          // Make sound working
-          var linkSoundUk = oaldResult.find('.audio_play_button.pron-uk').attr('data-src-mp3');
-          var soundHtmlUk = '<audio id="sound1" src="' + linkSoundUk + '" preload="auto"></audio>';
-          oaldResult.find('.audio_play_button.pron-uk').attr('onclick', 'document.getElementById("sound1").play()');
-          var linkSoundUs = oaldResult.find('.audio_play_button.pron-us').attr('data-src-mp3');
-          var soundHtmlUs = '<audio id="sound2" src="' + linkSoundUs + '" preload="auto"></audio>';
-          var html = oaldResult.append(soundHtmlUk).append(soundHtmlUs);
-          oaldResult.find('.audio_play_button.pron-us').attr('onclick', 'document.getElementById("sound2").play()');
-          $('#oald').append(oaldResult);
+      }).done(function(data) {
+        $("#spinning").empty();
+        if (!data.oaldData) {
+          $("#oald").append("CANT FIND ANY WORD!");
+        }
+        // Get OALD data
+        var oaldResult = $(data.oaldData);
+        // Make sound working
+        var linkSoundUk = oaldResult
+          .find(".audio_play_button.pron-uk")
+          .attr("data-src-mp3");
+        var soundHtmlUk =
+          '<audio id="sound1" src="' +
+          linkSoundUk +
+          '" preload="auto"></audio>';
+        oaldResult
+          .find(".audio_play_button.pron-uk")
+          .attr("onclick", 'document.getElementById("sound1").play()');
+        var linkSoundUs = oaldResult
+          .find(".audio_play_button.pron-us")
+          .attr("data-src-mp3");
+        var soundHtmlUs =
+          '<audio id="sound2" src="' +
+          linkSoundUs +
+          '" preload="auto"></audio>';
+        var html = oaldResult.append(soundHtmlUk).append(soundHtmlUs);
+        oaldResult
+          .find(".audio_play_button.pron-us")
+          .attr("onclick", 'document.getElementById("sound2").play()');
+        $("#oald").append(oaldResult);
 
-          // Get 1tudien data
-          var vndicResult = data.evData;
-          $('#vndic').append(vndicResult);
-        });
+        // Get 1tudien data
+        var vndicResult = data.evData;
+        $("#vndic").append(vndicResult);
+      });
     }
   });
 });
